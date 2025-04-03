e3a824407f58299f31e8622ef02648fce8ef3cd9
David Moreno <elarreglador@protonmail.com>
2025-04-03 15:29:08 +0200

    wifi_conect pasa a ser un componente

--- build/esp-idf/cmake_install.cmake
+++ build/esp-idf/cmake_install.cmake
@@ -412,3 +412,8 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("/home/david/esp/wifi01/build/esp-idf/main/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/david/esp/wifi01/build/esp-idf/wifi_conect/cmake_install.cmake")
+endif()
+
